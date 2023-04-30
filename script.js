e4037c7667e08204a0cfa60cc280e8665257d34b
Min Thiha Maung <tonymaung01@gmail.com>
2023-04-30 14:53:57 +0630

    First Commit

---
+++ script.js
@@ -0,0 +1,5 @@
+const imageContainer = document.querySelector(".main-img");
+const image = document.querySelector(".img");
+image.addEventListener("click", function () {
+  image.classList.add("grow");
+});
